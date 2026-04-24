0d7052fd48218dd3ce025ad0bf34ba91ed81ae6d
Atlasis Antonis <atantou@aol.com>
2026-04-24 15:24:55 +0300

    chore: update next.config.ts configuration settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        '*.app.github.dev' // Επιτρέπει όλα τα URL του GitHub Codespaces
+      ],
+    },
+  },
 };
 
 export default nextConfig;
